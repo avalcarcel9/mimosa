2b11fb90bfaee6774ea613a694e19ac7f16d7a53
muschellij2 <muschellij2@gmail.com>
2017-08-28 11:01:16 -0400

    Passes R CMD check.  removed some empty examples.

--- R/get_weights.R
+++ R/get_weights.R
@@ -6,13 +6,30 @@
 #' @param sigma standard deviation of Gaussian kernel
 #' @export
 #' @return vector of weights
-#' @examples \dontrun{
-#' 
-#'}
+#' @examples
+#' offsets = c(-1, 0, 1)
+#' offsets = expand.grid(offsets, offsets, offsets)
+#' sigma = 3
+#' voxelDims = c(0.25, 0.8, 3)
+#' wts = get_weights(offsets = offsets, voxelDims = voxelDims
+#' sigma = sigma)
+#' stopifnot(length(wts)==27)
 get_weights <- function(offsets, voxelDims, sigma){
-    dists = t(apply(offsets, 1, function(x, y) x*y, y=voxelDims))
-    sqNorm = apply(dists*dists, 1, function(x) sum(x))
-    # Constant doesn't matter because we would rescale by
-    # max weight so that center voxel would be be 1
-    return(exp(-1*sqNorm/(2*sigma^2)))
+  ndim = length(voxelDims)
+  transpose = ncol(offsets) == ndim
+  dont_transpose = nrow(offsets) == ndim
+  if (!transpose && !dont_transpose) {
+    stop("Matrix of offsets are not correct!")
+  }
+  if (transpose) {
+    offsets = t(offsets)
+  }
+  offsets = offsets * voxelDims
+  sqNorm = colSums(offsets^2)
+
+  # dists = t(apply(offsets, 1, function(x, y) x*y, y=voxelDims))
+  # sqNorm = apply(dists*dists, 1, function(x) sum(x))
+  # Constant doesn't matter because we would rescale by
+  # max weight so that center voxel would be be 1
+  return(exp(-1*sqNorm/(2*sigma^2)))
 }
